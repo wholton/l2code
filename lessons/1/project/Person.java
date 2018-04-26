11b9e872a70d9e05b963e7f7ba93b87b3913cc28
Will <wholton@gatech.edu>
2018-04-25 23:22:08 -0400

    Start of lesson 1

---
+++ lessons/1/project/Person.java
@@ -0,0 +1,12 @@
+
+public class Person {
+
+	/*
+	 * Define variables to represent your age, sex, credit card balance, 
+	 * an 11 digit id (give yourself a random one), and if you have children
+	 */
+	public static void main(String[] args) {
+		System.out.println("Beginning calculations!");
+		// define your variables here, then print them out in the console.
+	}
+}
